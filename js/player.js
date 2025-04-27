b8cc2ac3674429123121986b9e1352eaf96e18f0
rgriola <94720986+rgriola@users.noreply.github.com>
2025-04-26 23:56:35 -0400

    update 3

--- js/player.js
+++ js/player.js
@@ -195,7 +195,10 @@ class Player extends Ball {
         ctx.beginPath();
         ctx.arc(x, y, this.radius, 0, Math.PI * 2);
         ctx.fillStyle = this.color;
+        ctx.strokeStyle = "orange";
+        ctx.lineWidth = 2;
         ctx.fill();
+        ctx.stroke();
         
         // Draw player name above the ball
         if (this.active) {
